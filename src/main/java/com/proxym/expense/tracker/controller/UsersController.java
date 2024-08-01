5bf5d03990c3507902493eb6eb8ae8097a4cd6f2
adem.thabti <adem.thabti@proxym-it.com>
2024-08-01 11:33:59 +0100

    add get user by id

--- src/main/java/com/proxym/expense/tracker/controller/UsersController.java
+++ src/main/java/com/proxym/expense/tracker/controller/UsersController.java
@@ -1,19 +1,47 @@
 package com.proxym.expense.tracker.controller;
 
 
+import com.proxym.expense.tracker.entities.UserEntity;
+import com.proxym.expense.tracker.services.users.UserService;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UsersController {
 
-    @GetMapping()
-    public ResponseEntity<String>getTest()
+    private  final UserService userService;
+
+    @GetMapping("")
+    public ResponseEntity<List<UserEntity>>getAllUsers()
     {
-        return new ResponseEntity<>("test", HttpStatus.OK);
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
+
+    @GetMapping("/{id}/{username}")
+    public ResponseEntity<UserEntity>getUser(@PathVariable("id") String id)
+    {
+        Optional<UserEntity> userEntity = userService.getUserById(Long.valueOf(id));
+        if(userEntity.isPresent()) {
+            return new ResponseEntity<>(userEntity.get(), HttpStatus.OK);
+        }
+            else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+
 }
+
+
